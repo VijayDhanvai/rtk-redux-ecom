dec46285064bfe5cdb60e514492907077cbf39d7
VijayDhanvai <dhanvai.vijay@gmail.com>
2026-03-23 20:08:53 +0530

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Cart from './components/Pages/Cart'
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      <Header />   
       <Routes>
         <Route path="/" element={<div className="bg-white p-8"><h1 className="text-3xl font-bold">Welcome to Our Store</h1></div>} />
         <Route path="/products" element={<Products />} />
